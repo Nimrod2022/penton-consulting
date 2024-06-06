bcaad3a1ec075a53250a20bd27aedca9731aa2f6
Nimrod2022 <nimrodmandelakibet@gmail.com>
2024-06-06 13:00:03 +0200

    mobile nav

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -14,27 +14,7 @@ const Home = ({ currentPage, setCurrentPage }: Props) => {
         <h1>Home</h1>
         <p>Welcome to the Home Page</p>
       </section>
-      <section id="services" className="py-20">
-        <h1>Services</h1>
-        <Button
-          targetPage={CurrentPage.Services}
-          setCurrentPage={setCurrentPage}
-        >
-          See All Services
-        </Button>
-      </section>
-      <section id="projects" className="py-20">
-        <h1>Projects</h1>
-      </section>
-      <section id="contact" className="py-20">
-        <h1>Contact</h1>
-        <Button
-          targetPage={CurrentPage.Contact}
-          setCurrentPage={setCurrentPage}
-        >
-          Get a Quote
-        </Button>
-      </section>
+      
     </div>
   );
 };
--- src/pages/navbar/Navbar.tsx
+++ src/pages/navbar/Navbar.tsx
@@ -2,6 +2,9 @@ import { CurrentPage } from "../../shared/types";
 import { Link } from "react-router-dom";
 import logo from "/assets/logo.svg";
 import Button from "../../shared/Button";
+import UseMediaQuery from "../../hooks/UseMediaQuery";
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/16/solid";
 
 type Props = {
   currentPage: CurrentPage;
@@ -9,52 +12,115 @@ type Props = {
   setCurrentPage: (value: CurrentPage) => void;
 };
 
-const Navbar = ({ currentPage,  setCurrentPage }: Props) => {
+const Navbar = ({ currentPage, setCurrentPage }: Props) => {
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+
+  const isAboveMediumScreens = UseMediaQuery("(min-width: 1060px)");
+
   return (
     <header>
       <nav>
         <div className="w-full fixed top-0 font-libre text-[#1E1E1E] py-6">
-          <div className="mx-auto w-5/6 flex items-center justify-between">
-            <div>
-              <Link to="/" onClick={() => setCurrentPage(CurrentPage.Home)}>
-                <img src={logo} alt="logo" />
-              </Link>
+          {isAboveMediumScreens ? (
+            <div className="mx-auto w-5/6 flex items-center justify-between">
+              <div>
+                <Link to="/" onClick={() => setCurrentPage(CurrentPage.Home)}>
+                  <img src={logo} alt="logo" />
+                </Link>
+              </div>
+              <div className=" flex  gap-10 text-lg">
+                <Link to="/" onClick={() => setCurrentPage(CurrentPage.Home)}>
+                  Home
+                </Link>
+                <Link
+                  to="/services"
+                  onClick={() => setCurrentPage(CurrentPage.Services)}
+                >
+                  Services
+                </Link>
+                <Link
+                  to="/projects"
+                  onClick={() => setCurrentPage(CurrentPage.Projects)}
+                >
+                  Projects
+                </Link>
+                <Link
+                  to="/contact"
+                  onClick={() => setCurrentPage(CurrentPage.Contact)}
+                >
+                  Contact
+                </Link>
+              </div>
+              <div>
+                <Button
+                  targetPage={CurrentPage.Contact}
+                  setCurrentPage={setCurrentPage}
+                  bgColor="bg-[#003366]"
+                  textColor="text-white"
+                >
+                  Request a Quote
+                </Button>
+              </div>
             </div>
-            <div className="flex gap-10">
-              <Link to="/" onClick={() => setCurrentPage(CurrentPage.Home)}>
-                Home
-              </Link>
-              <Link
-                to="/services"
-                onClick={() => setCurrentPage(CurrentPage.Services)}
-              >
-                Services
-              </Link>
-              <Link
-                to="/projects"
-                onClick={() => setCurrentPage(CurrentPage.Projects)}
-              >
-                Projects
-              </Link>
-              <Link
-                to="/contact"
-                onClick={() => setCurrentPage(CurrentPage.Contact)}
-              >
-                Contact
-              </Link>
+          ) : (
+            <div className="flex items-center">
+              <div>
+                <Link to="/" onClick={() => setCurrentPage(CurrentPage.Home)}>
+                  <img
+                    src={logo}
+                    alt="logo"
+                    className="h-16 fixed left-5 top- "
+                  />
+                </Link>
+              </div>
+              <div>
+                <button
+                  className="rounded-full bg-[#003366] fixed right-5 top-0 z-40  p-2 my-4"
+                  onClick={() => setIsMenuToggled(!isMenuToggled)}
+                >
+                  <Bars3Icon className="size-6 text-white" />
+                </button>
+              </div>
             </div>
-            <div>
-              <Button
-                targetPage={CurrentPage.Contact}
-                setCurrentPage={setCurrentPage}
-                bgColor="bg-[#003366]"
-                textColor="text-white"
-              >
-                Request a Quote
-              </Button>
+          )}
+        </div>
+        {!isAboveMediumScreens && isMenuToggled && (
+          // Mobile Menu
+          <div className="relative">
+            <button
+              onClick={() => setIsMenuToggled(!isMenuToggled)}
+              className="fixed top-4 right-5 bg-[#003366] rounded-full z-50"
+            >
+              <XMarkIcon className="size-10 text-rose-400" />
+            </button>
+
+            <div className="fixed inset-0 bg-[#1E568E] bg-opacity-20 z-40 h-screen drop-shadow-xl">
+              <div className="flex flex-col text-xl justify-center h-full duration-300 ease-in-out items-center gap-5">
+                <Link to="/" onClick={() => setCurrentPage(CurrentPage.Home)}>
+                  Home
+                </Link>
+                <Link
+                  to="/services"
+                  onClick={() => setCurrentPage(CurrentPage.Services)}
+                >
+                  Services
+                </Link>
+                <Link
+                  to="/projects"
+                  onClick={() => setCurrentPage(CurrentPage.Projects)}
+                >
+                  Projects
+                </Link>
+                <Link
+                  to="/contact"
+                  onClick={() => setCurrentPage(CurrentPage.Contact)}
+                >
+                  Contact
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
+        )}
       </nav>
     </header>
   );
