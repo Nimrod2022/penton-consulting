5f17c51881b88ffb3b4fc945a30baf23077dc370
Nimrod2022 <nimrodmandelakibet@gmail.com>
2024-06-10 15:29:23 +0200

    floating navbar desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useSelector } from 'react-redux';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { RootState } from './app/store';
 import Contact from './pages/contact/Contact';
 import Home from './pages/home/Home';
 import Navbar from './pages/navbar/Navbar';
@@ -8,13 +6,13 @@ import Projects from './pages/projects/Projects';
 import Services from './pages/services/Services';
 
 function App() {
-  const currentPage = useSelector((state: RootState) => state.page.page);
+  // const currentPage = useSelector((state: RootState) => state.page.page);
 
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home currentPage={currentPage} />} />
+        <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
--- src/pages/navbar/Navbar.tsx
+++ src/pages/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/16/solid';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import logo from '../../../public/assets/logo.svg';
@@ -13,12 +13,34 @@ const Navbar = () => {
   const dispatch = useDispatch();
   const isAboveMediumScreens = UseMediaQuery('(min-width: 1060px)');
 
+useEffect(() => {
+  const handleScroll = () => {
+    const navbar = document.querySelector('.navbar');
+    if (navbar) {
+      if (window.scrollY > 0) {
+        navbar.classList.add('floating');
+      } else {
+        navbar.classList.remove('floating');
+      }
+    }
+  };
+
+  // Call handleScroll once on mount to set initial state
+  handleScroll();
+
+  window.addEventListener('scroll', handleScroll);
+
+  return () => {
+    window.removeEventListener('scroll', handleScroll);
+  };
+}, []);
+
   return (
     <header>
       <nav>
-        <div className="w-full fixed top-0 font-libre text-[#1E1E1E] ">
+        <div className="w-full  fixed top-0 font-libre text-[#1E1E1E] ">
           {isAboveMediumScreens ? (
-            <div className="mx-auto w-5/6 flex items-center justify-between">
+            <div className="mx-auto bg-white navbar w-5/6 flex items-center justify-between">
               <div>
                 <Link
                   to="/"
@@ -89,41 +111,49 @@ const Navbar = () => {
         </div>
         {!isAboveMediumScreens && isMenuToggled && (
           // Mobile Menu
-          <div className="relative">
-            <button
-              aria-label="Toggle Menu"
-              onClick={() => setIsMenuToggled(!isMenuToggled)}
-              className="fixed top-4 right-5 bg-[#003366] rounded-full z-50"
-            >
-              <XMarkIcon className="size-10 text-rose-400" />
-            </button>
+          <div className="navbar bg-white top-0 fixed">
+            <div className="relative">
+              <button
+                aria-label="Toggle Menu"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+                className="fixed top-4 right-5 bg-[#003366] rounded-full z-50"
+              >
+                <XMarkIcon className="size-10 text-rose-400" />
+              </button>
 
-            <div className="fixed inset-0 bg-[#1E568E] bg-opacity-20 z-40 h-screen drop-shadow-xl">
-              <div className="flex flex-col text-xl justify-center h-full duration-300 ease-in-out items-center gap-5">
-                <Link
-                  to="/"
-                  onClick={() => dispatch(setCurrentPage(CurrentPage.Home))}
-                >
-                  Home
-                </Link>
-                <Link
-                  to="/services"
-                  onClick={() => dispatch(setCurrentPage(CurrentPage.Services))}
-                >
-                  Services
-                </Link>
-                <Link
-                  to="/projects"
-                  onClick={() => dispatch(setCurrentPage(CurrentPage.Services))}
-                >
-                  Projects
-                </Link>
-                <Link
-                  to="/contact"
-                  onClick={() => dispatch(setCurrentPage(CurrentPage.Contact))}
-                >
-                  Contact
-                </Link>
+              <div className="fixed inset-0 bg-[#1E568E] bg-opacity-20 z-40 h-screen drop-shadow-xl">
+                <div className="flex flex-col text-xl justify-center h-full duration-300 ease-in-out items-center gap-5">
+                  <Link
+                    to="/"
+                    onClick={() => dispatch(setCurrentPage(CurrentPage.Home))}
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    to="/services"
+                    onClick={() =>
+                      dispatch(setCurrentPage(CurrentPage.Services))
+                    }
+                  >
+                    Services
+                  </Link>
+                  <Link
+                    to="/projects"
+                    onClick={() =>
+                      dispatch(setCurrentPage(CurrentPage.Services))
+                    }
+                  >
+                    Projects
+                  </Link>
+                  <Link
+                    to="/contact"
+                    onClick={() =>
+                      dispatch(setCurrentPage(CurrentPage.Contact))
+                    }
+                  >
+                    Contact
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
