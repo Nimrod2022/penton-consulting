4ae25ea9d4fa197732583ce327c704962d2a1340
Nimrod2022 <nimrodmandelakibet@gmail.com>
2024-06-11 15:34:22 +0200

    operation  - min tile height

--- src/components/Operations.tsx
+++ src/components/Operations.tsx
@@ -6,13 +6,17 @@ type Props = {
 
 const Operations = ({ image, title, description }: Props) => {
   return (
-    <div className="bg-[#0E4379] md:w-[380px] md:h-[420px] flex flex-col md:px-5 md:py-10 items-center ">
-      <img src={image} alt={title} className="size-20" />
-      <h3 className="text-xl py-5 text-center">{title}</h3>
-      <p className="text-[#DBE3EB]">{description}</p>
-    </div>
+    // <div className="bg-[#0E4379] md:w-[380px] md:h-[420px] flex flex-col md:px-5 md:py-10 items-center ">
+    //   <img src={image} alt={title} className="size-20" />
+    //   <h3 className="text-xl py-5 text-center">{title}</h3>
+    //   <p className="text-[#DBE3EB]">{description}</p>
+    // </div>
 
-    
+    <div className="bg-[#0E4379] md:w-full md:max-w-[380px] md:min-h-[450px] md:h-auto flex flex-col md:px-5 md:py-10 items-center ">
+      <img src={image} alt={title} className="" />
+      <h3 className="text-xl py-5 text-center">{title}</h3>
+      <p className="text-[#DBE3EB] text-center">{description}</p>
+    </div>
   );
 };
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -62,7 +62,7 @@ const Home = () => {
             </div>
           </div>
 
-          <div className="md:flex gap-x-10 justify-between mx-auto md:pb-20  grid xs:2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <div className="md:flex gap-x-10 justify-between mx-auto md:pb-20  grid xs:grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
             <div>
               <Operations
                 image={safetyOperation}
