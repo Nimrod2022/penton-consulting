fe6b193f9c88253303c8ef7bfb113a967b060671
Nimrod2022 <nimrodmandelakibet@gmail.com>
2024-06-11 14:40:47 +0200

    opeation cutom tile

--- src/components/Operations.tsx
+++ src/components/Operations.tsx
@@ -12,7 +12,7 @@ const Operations = ({ image, title, description }: Props) => {
     //   <p className="text-[#DBE3EB]">{description}</p>
     // </div>
 
-    <div className="bg-[#0E4379] w-full md:w-5/6 p-4 flex flex-col items-center">
+    <div className="bg-[#0E4379] w-full md:w-[90%] xl:w-5/6 p-4 flex flex-col items-center">
       <div className="flex flex-col md:px-5 md:py-10 items-center">
         <img
           src={image}
@@ -20,7 +20,7 @@ const Operations = ({ image, title, description }: Props) => {
           className=" object-cover"
         />
         <h3 className="text-xl py-5 text-center text-white">{title}</h3>
-        <p className="text-[#DBE3EB]">{description}</p>
+        <p className="text-[#DBE3EB] ">{description}</p>
       </div>
     </div>
   );
