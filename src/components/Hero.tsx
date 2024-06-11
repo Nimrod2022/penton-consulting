151e76240ab4f209e932ba20ffa7cea1163614c1
Nimrod2022 <nimrodmandelakibet@gmail.com>
2024-06-11 09:48:04 +0200

    preloading landing image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,36 +1,49 @@
 import Button from './buttons/Button';
 import landingImage from '../../public/assets/landing-bg.svg';
+import landingMobileImage from '../../public/assets/landing-mobile.png';
 import { useAppSelector } from '../hooks/UseApp';
 import { CurrentPage } from '../shared/types';
-
+import { useEffect } from 'react';
 
 const Hero = () => {
   const currentPage = useAppSelector((state) => state.page.page);
   console.log('Current page in Hero component:', currentPage);
+
+  useEffect(() => {
+    // Preload images
+    const img = new Image();
+    img.src = landingImage;
+    const imgMobile = new Image();
+    imgMobile.src = landingMobileImage;
+  }, []);
+
   return (
     <section className="w-full">
       <div>
         <div>
+          {/* Use low-quality placeholder */}
           <img
             src={landingImage}
             alt="home"
             className="md:pt-5 hidden md:block md:w-full"
+            loading="lazy"
           />
           <img
-            src="../../public/assets/landing-mobile.png"
+            src={landingMobileImage}
             alt="home"
             className="md:pt-5 md:hidden block md:w-full h-4/6"
+            loading="lazy"
           />
         </div>
-        <div className="w-full bg-[#003366] ">
-          <div className="text-white   md:py-20  md:flex  md:w-5/6 xl:w-4/6 mx-auto items-center">
+        <div className="w-full bg-[#003366]">
+          <div className="text-white md:py-20 md:flex md:w-5/6 xl:w-4/6 mx-auto items-center">
             <div className="md:pt-0 pt-5">
-              <h1 className="md:text-4xl text-xl md:px-0 px-10  md:text-start text-center font-libre">
+              <h1 className="md:text-4xl text-xl md:px-0 px-10 md:text-start text-center font-libre">
                 Where Innovation Meets Energy Excellence
               </h1>
             </div>
             <div className="md:py-0 px-5 py-5">
-              <div className="md:w-5/6 ">
+              <div className="md:w-5/6">
                 <p className="md:w-5/6 text-center md:text-start">
                   Penton is more than a service provider; we&apos;re your
                   trusted energy partner. With our expertise, reliability, and
