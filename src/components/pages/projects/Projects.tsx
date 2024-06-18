79babc8c868e61e50be58edac97fa6905be5f7f2
Nimrod2022 <nimrodmandelakibet@gmail.com>
2024-06-18 18:49:32 +0200

    project page message

--- src/components/pages/projects/Projects.tsx
+++ src/components/pages/projects/Projects.tsx
@@ -1,9 +1,8 @@
 const Projects = () => {
   return (
-    <section id="projects" className="py-20 mx-auto min-h-full w-5/6">
-      <div>
-        <h1>Home</h1>
-        <p>Welcome to the Projects Page</p>
+    <section id="projects" className="py-20 mx-auto min-h-full w-5/6 ">
+      <div className="flex justify-center md:pt-40 pt-20">
+        <p>COOMING SOON...</p>
       </div>
     </section>
   );
